915b4ad90da673b734a817dcefb60cece9cd699f
alienperfect <alienperfecthq@gmail.com>
2022-05-04 23:35:15 +0300

    signup view

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-from account.views import CustomLoginView, CustomLogoutView
+from account.views import SignUpView, CustomLoginView
+from django.contrib.auth.views import LogoutView
+from django.urls import path, reverse_lazy
 
 app_name = 'account'
 urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('dictionary:main')), name='logout'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,23 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'account/signup.html'
+
+    def form_valid(self, form):
+        """Saves the model and logs the user in."""
+        form.save()
+        login(self.request, form.instance)
+        return HttpResponseRedirect(reverse_lazy('dictionary:main'))
+
 
 class CustomLoginView(LoginView):
     template_name = 'account/login.html'
     next_page = reverse_lazy('dictionary:main')
     redirect_authenticated_user = True
-
-
-class CustomLogoutView(LogoutView):
-    next_page = reverse_lazy('dictionary:main')
